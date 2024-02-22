34ed454ee38762be4c787d72b7a6a130844c3b07
Mirko Popovic <popovicmirko78@gmail.com>
2024-02-22 17:39:01 +0100

    first commit

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div class="container">
+            <form action="POST" class="form" id="login">
+                <div class="from-content">
+                    <h2>login</h2>
+                    <input class="text-input" type="text" name="username" id="username" placeholder="username"><br>
+                    <input class="text-input"type="password" name="password" id="password" placeholder="password"><br>
+                    <input class="button" type="submit" value="login">
+                    <p><a href="register.php">register</a></p>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div class="container">
+            <form action="POST" class="form" id="login">
+                <div class="from-content">
+                    <h2>register</h2>
+                    <input class="text-input" type="text" name="name" id="name" placeholder="name"><br>
+                    <input class="text-input" type="text" name="surname" id="surname" placeholder="surname"><br>
+                    <input class="text-input" type="email" name="email" id="email" placeholder="email"><br>
+                    <input class="text-input" type="text" name="username" id="username" placeholder="username"><br>
+                    <input class="text-input"type="password" name="password" id="password" placeholder="password"><br>
+                    <input class="button" type="submit" value="register">
+                    <p><a href="login.php">login</a></p>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
